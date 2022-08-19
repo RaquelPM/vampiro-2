2ffc627cbd273a56dd117af1bcda8fa034d3c575
adissontejo <adissonmprtejo@gmail.com>
2022-08-19 18:49:47 -0300

    Add splash screen

--- android/app/src/main/java/com/vampiro/MainActivity.java
+++ android/app/src/main/java/com/vampiro/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
 
@@ -18,7 +19,8 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
+    RNBootSplash.init(this);
+    super.onCreate(savedInstanceState);
   }
 
   /**
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import RNBootSplash from 'react-native-bootsplash';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { ContextProvider } from './contexts';
@@ -8,6 +9,10 @@ import { Routes } from './routes';
 import { ThemesProvider } from './styles';
 
 export const App = () => {
+  useEffect(() => {
+    RNBootSplash.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <ThemesProvider>
